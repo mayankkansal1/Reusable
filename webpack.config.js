febc83c4a07ac591630b8328e1ee4722c0871e07
mayank-d2i <152134639+mayank-d2i@users.noreply.github.com>
2025-01-09 18:48:51 +0530

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,41 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    entry: './src/main.ts',
-    mode:process.env.NODE_ENV === "production" ? "production" : "development",
-    module:{rules:[
-        {
-            test:/\.svg$/,
-            use: 'svg-inline-loader'
-        },
-        {
-            test:/\.css$/i,
-            use:['style-loader','css-loader']
-        },
-        {
-            test:/\.(?:js|ts|jsx|tsx)$/,
-            use: "babel-loader"
-        }
-    ]},
-    output:{
-        path: path.resolve(__dirname,'dist'),
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        compress: true,
+        port: 3000,
+        historyApiFallback: true, // Useful for single-page apps
+        open: true // Opens the browser after starting the server
+    },
+    entry: './src/index.tsx',
+    mode: process.env.NODE_ENV === "production" ? "production" : "development",
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
+                use: 'svg-inline-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(?:js|ts|jsx|tsx)$/,
+                use: "babel-loader"
+            }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     // plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
     plugins: [new HtmlWebpackPlugin()],
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()]
+    }
 }
