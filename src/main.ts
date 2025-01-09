818876bc6d04dac91f696c776f030c3d9811cd76
mayank <mayankkansal1@gmail.com>
2025-01-09 09:27:18 +0530

    webpack configs

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+const myArray = ["Mercury", "Venus", "Earth", "Mars"];
+
+console.log(myArray); 
+console.log(myArray[0]);
+
+export default myArray;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
-    entry: './src/index.js',
-    
+    entry: './src/main.ts',
+    mode:process.env.NODE_ENV === "production" ? "production" : "development",
+    module:{rules:[
+        {
+            test:/\.svg$/,
+            use: 'svg-inline-loader'
+        },
+        {
+            test:/\.css$/i,
+            use:['style-loader','css-loader']
+        },
+        {
+            test:/\.(?:js|ts|jsx|tsx)$/,
+            use: "babel-loader"
+        }
+    ]},
+    output:{
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.js'
+    },
+    // plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+    plugins: [new HtmlWebpackPlugin()],
 }
